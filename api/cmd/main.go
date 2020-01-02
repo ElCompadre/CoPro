29eb865738a8b84f47f3c6e7e648942387460213
maxime <maxime.gauthier@portima.com>
2020-01-02 11:07:14 +0100

    init

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,15 +1,56 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"log"
+	"github.com/lib/pq"
+	"os"
+	"github.com/jinzhu/gorm"
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	handleRequest()
+
+var db *gorm.DB
+var err error
+
+type Resource struct {
+    gorm.Model
+
+    Link        string
+    Name        string
+    Author      string
+    Description string
+    Tags        pq.StringArray `gorm:"type:varchar(64)[]"`
 }
 
-func handleRequest() {
-	myRouter := mux.NewRouter()
+func main() {
+    router := mux.NewRouter()
 
-	myRouter.HandleFunc("api/users").Methods("GET")
+    db, err = gorm.Open(
+        "postgres",
+        "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+
+        " dbname="+os.Getenv("DBNAME")+" sslmode=disable password="+ 
+        os.Getenv("PASSWORD"))
+
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    defer db.Close()
+
+    db.AutoMigrate(&Resource{})
+
+    router.HandleFunc("/resources", GetResources).Methods("GET")
+    // router.HandleFunc("/resources/{id}", GetResource).Methods("GET")
+    // router.HandleFunc("/resources", CreateResource).Methods("POST")
+    // router.HandleFunc("/resources/{id}", DeleteResource).Methods("DELETE")
+
+    log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
+
+func GetResources(w http.ResponseWriter, r *http.Request) {
+    var resources []Resource
+    db.Find(&resources)
+    json.NewEncoder(w).Encode(&resources)
 }
